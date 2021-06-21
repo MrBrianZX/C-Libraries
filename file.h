d452c11fe8831640d59cc53011d787f23feac3ae
Brian Soto <briansotozx@gmail.com>
2021-06-20 17:11:28 -0700

    Commit Inicial

---
+++ file.h
@@ -0,0 +1,10 @@
+//
+// Created by Brian on 20/06/2021.
+//
+
+#ifndef UNTITLED_FILE_H
+#define UNTITLED_FILE_H
+
+
+
+#endif //UNTITLED_FILE_H
---
+++ main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int tacos();
+
+int main() {
+    printf("Hello, World!\n");
+    tacos();
+    return 0;
+}
+
+int tacos(){
+    printf("protoman");
+    return 0;
+}
